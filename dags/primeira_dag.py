e6b226c3d51b969d615e52d137fbab9e07edf5fa
Yan Robim <yanrobim@gmail.com>
2025-04-04 08:49:26 -0300

    removendo .env

---
+++ dags/primeira_dag.py
@@ -0,0 +1,15 @@
+# O Professor ensina 3 maneiras de criar uma DAG. Essa é a primeira sendo com um with, com esse with abrimos e fechamos um processo automaticamente, esse processo sendo a class DAG.
+
+
+import datetime
+
+from airflow import DAG
+from airflow.operators.empty import EmptyOperator
+
+with DAG(
+dag_id="meu_nome_de_dag",
+start_date=datetime.datetime(2021, 1, 1),
+schedule="@daily",
+catchup=False
+):
+    EmptyOperator(task_id="tarefa")
---
+++ dags/segunda_dag.py
@@ -0,0 +1,15 @@
+# O Professor ensina 3 maneiras de criar uma DAG. Essa é a segunda sendo com uma instância (variável ), a nossa class DAG está sendo armazenada em uma instância e no final sendo executada por um Operador.
+
+
+import datetime
+
+from airflow import DAG
+from airflow.operators.empty import EmptyOperator
+
+minha_dag = DAG(
+    dag_id="meu_nome_de_dag",
+    start_date=datetime.datetime(2025, 4, 4),
+    schedule="@daily",
+    catchup=False, # Adiciona uma linha
+)
+EmptyOperator(task_id="tarefa", dag=minha_dag)
